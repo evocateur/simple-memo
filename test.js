53c5b59cf364f27735e6d74027966cf9f53148f7
Daniel Stockman <daniel.stockman@gmail.com>
2014-06-03 12:17:12 -0700

    Add mocha+chai+sinon test apparatus.

---
+++ test.js
@@ -0,0 +1,13 @@
+/*global describe, it */
+
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var should = chai.should();
+chai.use(sinonChai);
+
+var memoize = require("./");
+
+describe("export", function () {
+    it("should work");
+});
